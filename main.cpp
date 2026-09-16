c308ffaa807e04aadd4cbc6eb13eb73f50f1c0e9
Arilr269 <ariloro269@gmail.com>
2026-09-16 16:38:12 -0600

    2

--- main.cpp
+++ main.cpp
@@ -74,6 +74,24 @@ void mostrarTareas(const vector<Tarea>& tareas) {
 }
 
 // Marca una tarea como completada
-// void completarTarea(vector<Tarea>& tareas) {
-// 
-// }
+void completarTarea(vector<Tarea>& tareas) {
+// Imprimir tareas
+    mostrarTareas(tareas);
+
+
+// Cambiar tarea completada a true
+
+// Numero de tarea a completar
+    int numeroTarea;
+    cout << "Seleccione la tarea completada: ";
+    cin >> numeroTarea;
+    cin.ignore();
+
+    if (numeroTarea < 1 || static_cast<size_t>(numeroTarea) > tareas.size()) {
+        cout << "TAREA INVALIDA." << endl;
+        return;
+    }
+
+    tareas[numeroTarea - 1].completada = true;
+    cout << "Tarea completada correctamente." << endl;
+}
